e61a7e7f102d69adf08c6887ff7ff514ddcfac66
Patience Koribirama <koribiram@yahoo.fr>
2024-03-08 10:37:11 +0100

    Filling the database with test data

--- src/DataFixtures/ProduitFixtures.php
+++ src/DataFixtures/ProduitFixtures.php
@@ -4,17 +4,42 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Produit;
+use App\Entity\Categorie;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class ProduitFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
-        $produit = new Produit();
-        $manager->persist($produit);
-
-        $manager->flush();
+        
+         // Create categories
+         $categories = [];
+         for ($i = 0; $i < 5; $i++) {
+             $categorie = new Categorie();
+             $categorie->setNomCategorie($faker->word())
+                       ->setImage($faker->imageUrl())
+                       ->setDescriptionCategorie($faker->paragraph());
+ 
+             $manager->persist($categorie);
+             $categories[] = $categorie;
+         }
+ 
+         // Création de plusieurs produits
+         for ($i = 0; $i < 20; $i++) {
+             $produit = new Produit();
+             $produit->setNomProduit($faker->lastName())
+                     ->setImage($faker->imageUrl())
+                     ->setPrix($faker->randomFloat(2, 10, 100))
+                     ->setQuantiteStock($faker->numberBetween(0, 100))
+                     ->setDateCreation($faker->dateTimeBetween('-1 year', 'now'))
+                     ->setDescriptionCourte($faker->paragraph())
+                     ->setCategorie($faker->randomElement($categories));
+ 
+             $manager->persist($produit);
+         }
+ 
+         $manager->flush();
     }
 }
