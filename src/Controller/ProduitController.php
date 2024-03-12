3392310f611e45c1397921f72d40de11e4060cc0
Patience Koribirama <koribiram@yahoo.fr>
2024-03-12 01:56:52 +0100

    Creating the form and update route but not finalizing

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Produit;
 use App\Form\ProduitType;
+use App\Form\ProduitUpdateType;
 use App\Repository\ProduitRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,9 +24,9 @@ class ProduitController extends AbstractController
         ]);
     }
 
-       /**
-     * @route("/create" , name="create")
-     */
+    /**
+    * @route("/create" , name="create")
+    */
     public function create(Request $request): Response
     {
         $produit = new Produit;
@@ -39,10 +40,45 @@ class ProduitController extends AbstractController
             $sendDatabase->flush();
 
             $this->addFlash('notice', 'Votre produit a été ajouté !!'); 
-            return $this->redirectToRoute('app_accueil'); // Gère la redirection vers la page d'accueil après validation du formulaire
+            return $this->redirectToRoute('showTable'); // Gère la redirection vers la page d'accueil après validation du formulaire
         }
         return $this->render('produit/formProduit.html.twig', [
             'form' => $form->createView()
         ]);
-}
+    }
+
+    /**
+    * @route("/update/{id}" , name="update")
+    */
+    public function update(Request $request, $id): Response
+    {
+        $produit = $this->getDoctrine()->getRepository(Produit::class)->find($id);  
+        $form = $this->createForm(ProduitUpdateType::class, $produit); // creation du form
+        $form -> handleRequest($request); 
+
+        if ( $form->isSubmitted() && $form->isValid()) {
+            $sendDatabase = $this->getDoctrine()
+                                 ->getManager();
+            $sendDatabase->persist($produit);
+            $sendDatabase->flush();
+
+            $this->addFlash('notice', 'Votre produit a été modifié !!'); 
+            return $this->redirectToRoute('showTable'); // Gère la redirection vers la page d'accueil après validation du formulaire
+        }
+        return $this->render('produit/upDateProduit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @route("/table", name= "showTable")
+     */
+    public function showTable(ProduitRepository  $repo):Response
+    {
+        $table = $repo->findAll();
+        return $this->render('produit/tableProduits.html.twig', [
+            'controller_name' => 'MainController',
+            "datas_table" => $table,
+        ]);
+    }
 }
--- src/Form/ProduitUpdateType.php
+++ src/Form/ProduitUpdateType.php
@@ -9,37 +9,37 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
-class ProduitType extends AbstractType
+class ProduitUpdateType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nomProduit')
+            ->add('prix')
+            ->add('quantiteStock')
+            ->add('dateCreation')
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'nomCategorie',
+            ])
+            ->add('descriptionCourte')
             ->add('imageFile', VichImageType::class, [
-                'label' => 'Image du produit',
                 'required' => false,
                 'allow_delete' => true,
                 'download_uri' => true,
                 'image_uri' => true,
                 'asset_helper' => true,
-            ])            
-            ->add('prix')
-            ->add('quantiteStock')
-            ->add('dateCreation' , DateTimeType::class, [
-                'date_label' => 'Starts On'])
-            ->add('descriptionCourte')
-            ->add('categorie', EntityType::class, [
-                'class' => Categorie::class,
-                'choice_label' => 'nomCategorie',
             ])
         ;
     }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Produit::class,
         ]);
     }
+
 }
