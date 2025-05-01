0413a05357fabba711ff72e9d2bcdd97411177e4
Patience-Kori1 <koribiram@yahoo.fr>
2025-05-01 16:54:18 +0200

    Changement Produit

--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -9,7 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<Produit>
  *
- * @method Produit|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Produit|null find($id, $lockMode z= null, $lockVersion = null)
  * @method Produit|null findOneBy(array $criteria, array $orderBy = null)
  * @method Produit[]    findAll()
  * @method Produit[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
